3f2444becdfe6772d9df8da0f0102e2f2453e4f1
fraginhell@gmail.com <keith>
2023-12-05 02:29:43 +0000

    playbook requirements

--- ubuntu_bootstrap.bash
+++ ubuntu_bootstrap.bash
@@ -1,4 +1,5 @@
-$ sudo apt update
-$ sudo apt install software-properties-common
-$ sudo add-apt-repository --yes --update ppa:ansible/ansible
-$ sudo apt install ansible
+sudo apt update
+sudo apt install git python3-psutil
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
